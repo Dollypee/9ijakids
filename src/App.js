86e8df6c8568f431c8b49e6ff9288e6d6d181ac5
Adedolapo Olaiya <olaiyadolapo@gmail.com>
2021-05-05 03:20:56 +0100

    Complete 9ija games task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
+import Home from './Views/Home';
+// import Game from './Views/Game';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          {/* <Route path="/games/:id">
+            <Game />
+          </Route> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
